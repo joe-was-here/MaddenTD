b7700ea88f86f315f1bc052522c2b0b1866a5114
Joe Madden <joe@beardbro.com>
2012-07-23 22:13:56 -0400

    changed some stuff for collision detection

--- js/script.js
+++ js/script.js
@@ -5,6 +5,7 @@ var canvas = document.getElementById('canvas'),
 	tick = 0,
 	blockedX = [],
     blockedY = [],
+    gridArray = [],
 	hp = 1,
 	unitId = 0,
 	offset = $('#canvas').offset(),
@@ -12,26 +13,45 @@ var canvas = document.getElementById('canvas'),
 	towers = [],
 	runScript = false;
 
+function Tower(type, topLeft, topRight, botRight, botLeft, towerHeight, towerWidth) {
+	this.type = type;
+	this.topLeft = topLeft;
+	this.topRight = topRight;
+	this.botRight = botRight;
+	this.botLeft = botLeft;
+	this.towerMid = [topLeft[1] + (towerWidth / 2), topLeft[0] + (towerHeight / 2)];
+};
 
 function buildPathingArrays(mapHeight, mapWidth, towerHeight, towerWidth) {
     var horizontalRows = mapHeight / towerHeight;
     var verticalRows = mapWidth / towerWidth;
     var xIterations = Math.floor(horizontalRows / 8);
     var yIterations = Math.floor(verticalRows / 8);
-    var xLeftover = horizontalRows % 8;
-    var yLeftover = verticalRows % 8;
+    var gridSections = horizontalRows * verticalRows;
+    var gridLoops = Math.floor(gridSections / 8);
+    var xRemainder = horizontalRows % 8;
+    var yRemainder = verticalRows % 8;
+    var gridRemainder = gridSections % 8;
     var i = 0;
     var x = 0;
     var y = 0;
+    var gridX = 1;
     var iY = 1;
     var iX = 1;
 
-    if (xLeftover > 0) {
+    for (var yTest = 1; yTest <= horizontalRows; yTest++) {
+    	for (var xTest = 1; xTest <= verticalRows; xTest++) {
+    		gridArray.push([yTest + ' x ' + xTest, 'empty']);
+    	}
+    }
+
+
+    if (xRemainder > 0) {
         do {
             blockedX[x] = towerWidth * iX;
             x++;
             iX++;
-        } while (--xLeftover > 0);
+        } while (--xRemainder > 0);
     }
 
     do {
@@ -45,12 +65,12 @@ function buildPathingArrays(mapHeight, mapWidth, towerHeight, towerWidth) {
         blockedX[x++] = towerWidth * x;
     } while (--xIterations > 0);
 
-    if (yLeftover > 0) {
+    if (yRemainder > 0) {
         do {
             blockedY[y] = towerHeight * iY;
             y++;
             iY++;
-        } while (--yLeftover > 0);
+        } while (--yRemainder > 0);
     }
 
     do {
@@ -66,6 +86,7 @@ function buildPathingArrays(mapHeight, mapWidth, towerHeight, towerWidth) {
     
     console.log(blockedX);
     console.log(blockedY);
+    console.log(gridArray);
 
 
 };
@@ -75,9 +96,10 @@ function generateUnits() {
     while (unitId < 100) {
         units.push({
             x: 0,
-            y: 350 - (Math.random() * 100), //between 250 and 350
-            speed: .5 + Math.random() * .4, //between 2 and 5
-            radius: 5 + Math.random() * 2, //between 5 and 10
+            y: 350 - (Math.random() * 100), //between 0 and canvas width
+            direction: 'right',
+            speed: .5+Math.random()*.4, //between 2 and 5
+            radius: 5+Math.random()*2, //between 5 and 10
             color: "black",
             id: unitId
         });
@@ -92,18 +114,42 @@ function updateUnits() {
 
 	for (var i in units) {
 		var part = units[i];
-		var nearestVerticalWall = part.speed > 0 ? Math.ceil(part.x / towerHeight) * towerHeight : Math.floor(part.x / towerHeight) * towerHeight;
-		var nearestHorizontalWall = part.speed > 0 ? Math.floor(part.y / towerWidth) * towerWidth : Math.ceil(part.y / towerWidth) * towerWidth;
-		var nearestVerticalWallArrayNum = nearestVerticalWall / 25;
-		var nearestHorizontalWallArrayNum = nearestHorizontalWall / 25;
-		var nearestVerticalWallBlocked = blockedY[nearestVerticalWallArrayNum] === 1 ? true : false;
-		var nearestHorizontalWallBlocked = blockedX[nearestHorizontalWallArrayNum] === 1 ? true : false;
+		var nearestVerticalSection = part.speed > 0 ? Math.ceil(part.x / towerHeight) * towerHeight : Math.floor(part.x / towerHeight) * towerHeight;
+		var nearestHorizontalSection = part.speed > 0 ? Math.floor(part.y / towerWidth) * towerWidth : Math.ceil(part.y / towerWidth) * towerWidth;
+		var nearestVerticalSectionArrayNum = nearestVerticalSection / 25;
+		var nearestHorizontalSectionArrayNum = nearestHorizontalSection / 25;
+		var nextGridSection = nearestVerticalSectionArrayNum + ' x ' + nearestHorizontalSectionArrayNum;
+		var nearestVerticalSectionBlocked = typeof blockedY[nearestVerticalSectionArrayNum] === 'object' ? true : false;
+		var nearestHorizontalSectionBlocked = typeof blockedX[nearestHorizontalSectionArrayNum] === 'object' ? true : false;
+		var direction;
+		var gridArrayNum;
+    	nearestHorizontalSectionArrayNum !== 1 ? gridArrayNum = ((nearestHorizontalSectionArrayNum * 44) + nearestVerticalSectionArrayNum) : gridArrayNum = nearestVerticalSectionArrayNum;
 
-		if (nearestVerticalWallBlocked === true && nearestHorizontalWallBlocked === true) {
-			part.y += part.speed;
+		if (typeof gridArray[gridArrayNum][1] === 'object') {
+			var blockingTowersMidX = gridArray[gridArrayNum][1].towerMid[1];
+			var blockingTowersMidY = gridArray[gridArrayNum][1].towerMid[0];
+			var keepGoing;
+
+			if (part.x >= blockingTowersMidX - 25 && part.y <= blockingTowersMidY) {
+				keepGoing = true;
+			} else {
+				keepGoing = false;
+			}
+				
+			console.log(keepGoing)
+
+			if (part.x >= (blockingTowersMidX - 25)) {
+				part.y += part.speed;
+			} else {
+				part.x += part.speed;
+			}
 		} else {
 			part.x += part.speed;
 		}
+
+		//if (part.direction !== 'right' || part.direction !== 'down') {
+		//	part.speed = -(part.speed);
+		//}
 	}
 };
 
@@ -152,7 +198,7 @@ function drawUnits() {
     ctx.fillRect(0,0,canvas.width,canvas.height);
     
 
-    for(var i in units) {
+    for (var i in units) {
         var part = units[i];
         ctx.beginPath();
         ctx.arc(part.x,part.y, part.radius, 0, Math.PI*2);
@@ -167,7 +213,7 @@ function drawUnits() {
 
 };
 
-function placeTower(clickedX, clickedY, towerWidth, towerHeight) {
+function placeTower(clickedX, clickedY, towerWidth, towerHeight, towerType) {
 	
 	if (clickedX == 0) {
 		clickedX = towerWidth;
@@ -180,21 +226,53 @@ function placeTower(clickedX, clickedY, towerWidth, towerHeight) {
 	if (clickedY == 0) {
 		clickedY = towerHeight;
 	} else if (clickedY > 0) {
-		clickedY = Math.ceil(clickedY/towerHeight) * towerHeight;
+		clickedY = Math.ceil(clickedY / towerHeight) * towerHeight;
 	} else if (clickedY == ctx.height) {
 		clickedY = ctx.height;
 	}
 
-	towers.push({
-		startingX: clickedX,
-		startingY: clickedY
-	});
-
+	var topLeft = [clickedX - 25, clickedY - 25];
+	var topRight = [clickedX, clickedY - 25];
+	var botRight = [clickedX, clickedY];
+	var botLeft = [clickedX - 25, clickedY];
+	var towerId = towerType + clickedX + clickedY;
     var xPosition = (clickedX / towerWidth) - 1;
     var yPosition = (clickedY / towerHeight) - 1;
+    var xPos = xPosition + 1;
+    var yPos = yPosition + 1;
+    var gridArrayNum;
 
-    blockedX[yPosition] = 1;
-    blockedY[xPosition] = 1;
+    yPos !== 1 ? gridArrayNum = ((yPosition * 44) + xPos) - 1 : gridArrayNum = xPosition;
+
+
+	if (typeof gridArray[gridArrayNum][1] === 'string') {
+
+		towers.push({
+			startingX: clickedX,
+			startingY: clickedY
+		});
+
+	    gridArray[gridArrayNum][1] = new Tower('arrow', topLeft, topRight, botRight, botLeft, towerHeight, towerWidth);
+
+	    console.log(gridArray);
+
+	}
+
+
+	// if (typeof blockedX[yPosition] === 'number' || typeof blockedY[xPosition] === 'number') {
+
+	// 	towers.push({
+	// 		startingX: clickedX,
+	// 		startingY: clickedY
+	// 	});
+
+	//     blockedX[yPosition] = new Tower('arrow', topLeft, topRight, botRight, botLeft, towerHeight, towerWidth);
+	//     blockedY[xPosition] = new Tower('arrow', topLeft, topRight, botRight, botLeft, towerHeight, towerWidth);
+
+	//     console.log(blockedX);
+ //        console.log(blockedY);
+
+	// }
 
 };
 
@@ -294,8 +372,6 @@ $(document).ready(function() {
 
 	$(canvas).mousedown(function() {
 		placeTower(mouseLocX, mouseLocY, 25, 25);
-		console.log(blockedX);
-        console.log(blockedY);
 	});
 
 	setInterval(loop,30);
